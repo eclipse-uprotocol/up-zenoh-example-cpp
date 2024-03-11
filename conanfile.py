2030072865ea5ae2f2ba3856fe7cc52480ba0d46
Michael <4115205+MelamudMichael@users.noreply.github.com>
2024-03-11 06:20:10 +0200

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -19,14 +19,10 @@ class HelloWorldRecipe(ConanFile):
         "build_cross_compiling": False,
     }
 
-    # def configure(self):
-    #     self.options["spdlog"].shared = self.options.shared
-
     def requirements(self):
         self.requires("up-client-zenoh-cpp/0.1.2-dev")
         self.requires("protobuf/3.21.12" + ("@cross/cross" if self.options.build_cross_compiling else ""))
             
-
     def imports(self):
         if self.options.build_testing:
             self.copy("*.so*", dst="lib", keep_path=False)
